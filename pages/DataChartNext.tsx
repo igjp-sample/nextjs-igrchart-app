c3e4f916e28f3fcfa60ab88e29a2b42a6b2deba2
MasakiTanoue <mtanoue@infragistics.com>
2023-11-28 20:46:02 +0900

    DataChartSynchronization

--- pages/DataChartNext.tsx
+++ pages/DataChartNext.tsx
@@ -1,100 +1,88 @@
 import React, { useRef } from "react";
-import { 
+import {
   FIgrDataChart, FIgrCategoryXAxis, FIgrNumericYAxis,
-  FIgrLineSeries, FIgrSplineSeries, FIgrStepLineSeries, 
+  FIgrLineSeries, FIgrSplineSeries, FIgrStepLineSeries,
 } from "../hooks/useDataChart";
-import { 
-  CountryRenewableElectricityItem, CountryRenewableElectricity 
+import {
+  CountryRenewableElectricityItem, CountryRenewableElectricity
 } from "../hooks/CountryRenewableElectricity";
+import { IgrDataChart } from "igniteui-react-charts";
 
-const DataChartNext = () => {
-  const dummyData: CountryRenewableElectricityItem[] = new CountryRenewableElectricity();
-  console.log(dummyData);
+class DataChartNext extends React.Component {
+  public dummyData: CountryRenewableElectricityItem[] = new CountryRenewableElectricity();
+  public chart: IgrDataChart;
+ 
+  constructor(props) {
+    super(props);
 
-  // useRefを使用してrefを作成
-  // const dataChartRef = useRef(null);
-  // useRefを使用してrefとしてonChartRefを作成
-  const onChartRef = useRef((chart) => {
-    // ここにchartが利用可能なロジックを追加
-    // 例：chart.syncChannel = "ChannelA";
+    console.log(this.dummyData);
+    this.onChartRef = this.onChartRef.bind(this);
+  }
+
+  onChartRef = (chart: IgrDataChart) => {
+    if (!chart) { return; }
+
+    chart.syncChannel = "channelA";
     chart.synchronizeHorizontally = true;
     chart.synchronizeVertically = true;
-  });
-  console.log(onChartRef);
+  };
 
-  return (
-    <div className="container sample">
-      <div className="container" style={{height: "100%"}}>
-      <FIgrDataChart 
-        ref={onChartRef.current}
-        width="800px"
-        height="500px"
-        dataSource={dummyData}
-        isHorizontalZoomEnabled={true}
-        isVerticalZoomEnabled={true}>
+  render() {
+    return (
+      <div className="container sample">
+        <div className="container" style={{ height: "100%" }}>
+          <FIgrDataChart
+            ref={this.onChartRef}
+            width="800px"
+            height="500px"
+            dataSource={this.dummyData}
+            isHorizontalZoomEnabled={true}
+            isVerticalZoomEnabled={true}
+          >
+            <FIgrCategoryXAxis name="xAxis" label="X" />
+            <FIgrNumericYAxis name="yAxis" />
+            <FIgrStepLineSeries
+              name="series3"
+              valueMemberPath="Russia"
+              xAxisName="xAxis"
+              yAxisName="yAxis"
+            />
+          </FIgrDataChart>
 
-        <FIgrCategoryXAxis
-          name="xAxis" 
-          label="X" 
-        ></FIgrCategoryXAxis>
-
-        <FIgrNumericYAxis 
-          name="yAxis" 
-        ></FIgrNumericYAxis>
-
-        <FIgrStepLineSeries 
-          name="series3" 
-          valueMemberPath="Russia"
-          xAxisName="xAxis" 
-          yAxisName="yAxis" 
-        ></FIgrStepLineSeries>
-
-      </FIgrDataChart>
-
-      <FIgrDataChart 
-        ref={onChartRef.current}
-        width="800px"
-        height="500px"
-        dataSource={dummyData}
-        isHorizontalZoomEnabled={true}
-        isVerticalZoomEnabled={true}>
-
-        <FIgrCategoryXAxis
-          name="xAxis" 
-          label="X" 
-        ></FIgrCategoryXAxis>
-
-        <FIgrNumericYAxis 
-          name="yAxis" 
-        ></FIgrNumericYAxis>
-
-        <FIgrLineSeries 
-          name="series1" 
-          title="USA"
-          valueMemberPath="USA"
-          xAxisName="xAxis"
-          yAxisName="yAxis"
-        ></FIgrLineSeries>
-
-        <FIgrSplineSeries 
-          name="series2" 
-          valueMemberPath="China"
-          xAxisName="xAxis"
-          yAxisName="yAxis" 
-        ></FIgrSplineSeries>
-
-        <FIgrStepLineSeries 
-          name="series3" 
-          valueMemberPath="Russia"
-          xAxisName="xAxis" 
-          yAxisName="yAxis" 
-        ></FIgrStepLineSeries>
-
-      </FIgrDataChart>
+          <FIgrDataChart
+            ref={this.onChartRef}
+            width="800px"
+            height="500px"
+            dataSource={this.dummyData}
+            isHorizontalZoomEnabled={true}
+            isVerticalZoomEnabled={true}
+          >
+            <FIgrCategoryXAxis name="xAxis" label="X" />
+            <FIgrNumericYAxis name="yAxis" />
+            <FIgrLineSeries
+              name="series1"
+              title="USA"
+              valueMemberPath="USA"
+              xAxisName="xAxis"
+              yAxisName="yAxis"
+            />
+            <FIgrSplineSeries
+              name="series2"
+              valueMemberPath="China"
+              xAxisName="xAxis"
+              yAxisName="yAxis"
+            />
+            <FIgrStepLineSeries
+              name="series3"
+              valueMemberPath="Russia"
+              xAxisName="xAxis"
+              yAxisName="yAxis"
+            />
+          </FIgrDataChart>
+        </div>
       </div>
-    </div>    
-  );
-
-};
+    );
+  }
+}
 
 export default DataChartNext;
