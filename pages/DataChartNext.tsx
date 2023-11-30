7fabcc22a2e8a192d91f2204c432dcdc26aff717
MasakiTanoue <mtanoue@infragistics.com>
2023-11-30 14:21:56 +0900

    Arrow Functions

--- pages/DataChartNext.tsx
+++ pages/DataChartNext.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   FIgrDataChart, FIgrCategoryXAxis, FIgrNumericYAxis,
   FIgrLineSeries, FIgrSplineSeries, FIgrStepLineSeries,
@@ -13,131 +13,119 @@ interface DataChartNextState {
   renderingText: string;
 }
 
-class DataChartNext extends React.Component<{}, DataChartNextState> {
-  public dummyData: CountryRenewableElectricityItem[] = new CountryRenewableElectricity();
-  public chartRef: IgrDataChart;
-  readonly highlightingMode = "FadeOthersSpecific";
+const DataChartNext: React.FC = () => {
+  const [chartRef, setChartRef] = useState<IgrDataChart | null>(null);
+  const [series1Thickness, setSeries1Thickness] = useState(1);
+  const [renderingText, setRenderingText] = useState("");
 
-  constructor(props) {
-    super(props);
+  const dummyData: CountryRenewableElectricityItem[] = new CountryRenewableElectricity();
+  const highlightingMode = "FadeOthersSpecific";
 
-    this.state = {
-      series1Thickness: 1,
-      renderingText: "", // 新しい state の初期値
-    };
-
-    this.onChartRef = this.onChartRef.bind(this);
-    this.handleHighlightButtonClick = this.handleHighlightButtonClick.bind(this);
-  }
-
-  onChartRef = (chart: IgrDataChart) => {
-    if (!chart) { return; }
-
-    this.chartRef = chart;
-    console.log("Step 1");
-    console.log("Chart series:", this.chartRef.series);
+  const onChartRef = (chart: IgrDataChart) => {
+    setChartRef(chart);
   };
 
-  componentDidMount() {
-  //   // コンポーネントがマウントされた後に handleHighlightButtonClick を呼び出す
-  //   this.handleHighlightButtonClick();
-  }
-  
-  handleHighlightButtonClick = () => {
+  const handleHighlightButtonClick = () => {
     console.log("Step 2");
   
-    if (this.chartRef) {
+    if (chartRef) {
       console.log("Step 3");
-  
-      const series1 = this.chartRef.series.findByName("series1") as IgrLineSeries;
-      // ここで series1 は null になる
-      console.log("series1:", series1);
+      console.log("chartRef:", chartRef);
+      console.log("chartRef.series:", chartRef.series);
 
+      // chartRef.seriesを配列に変換
+      const seriesArray = chartRef.series.toArray();
+      // デバッグ用にシリーズの名前を表示
+      seriesArray.forEach(series => console.log("Series name:", series.name));
+  
+      console.log("chartRef.series.item0:", chartRef.series.item(0));
+      console.log("chartRef.series.item1:", chartRef.series.item(1));
+      console.log("chartRef.series.item2:", chartRef.series.item(2));
+  
+      // "series1"を見つける
+      const series1 = seriesArray.find(series => series.name === "series1") as IgrLineSeries;
+  
       if (series1) {
         console.log("Step 4");
         // thickness の値を変更して線の太さを調整
         series1.thickness = 5;
   
-        // setState を使って状態を更新
-        this.setState({
-          series1Thickness: 5,
-          renderingText: "ボタンがクリックされました",
-        }, () => {
-          console.log("Step 5: setState is completed.");
-          console.log("Updated series1Thickness:", this.state.series1Thickness);
-          console.log("Updated series1 thickness:", series1.thickness);
+        // ローカル変数を更新
+        setSeries1Thickness(5);
+        setRenderingText("ボタンがクリックされました");
   
-          // ログを追加
-          console.log("Rendering text:", this.state.renderingText);
-        });
+        console.log("Step 5: setState is completed.");
+        console.log("Updated series1Thickness:", series1Thickness);
+        console.log("Updated series1 thickness:", series1.thickness);
+  
+        // ログを追加
+        console.log("Rendering text:", renderingText);
+      } else {
+        console.log("Series not found");
       }
     }
   };
+  
+  return (
+    <div className="container sample">
+      <div className="container" style={{ height: "100%" }}>
+        <FIgrDataChart
+          ref={onChartRef}
+          width="800px"
+          height="500px"
+          dataSource={dummyData}
+          isHorizontalZoomEnabled={true}
+          isVerticalZoomEnabled={true}
+        >
+          <FIgrCategoryXAxis name="xAxis" label="X" />
+          <FIgrNumericYAxis name="yAxis" />
+          <FIgrStepLineSeries
+            name="series3"
+            valueMemberPath="Russia"
+            xAxisName="xAxis"
+            yAxisName="yAxis"
+          />
+        </FIgrDataChart>
 
-  render() {
-    const { series1Thickness, renderingText } = this.state;
+        <FIgrDataChart
+          width="800px"
+          height="500px"
+          dataSource={dummyData}
+          isHorizontalZoomEnabled={true}
+          isVerticalZoomEnabled={true}
+          highlightingMode={highlightingMode}
+        >
+          <FIgrCategoryXAxis name="xAxis" label="X" />
+          <FIgrNumericYAxis name="yAxis" />
+          <FIgrLineSeries
+            name="series1"
+            title="USA"
+            valueMemberPath="USA"
+            xAxisName="xAxis"
+            yAxisName="yAxis"
+            thickness={series1Thickness}
+          />
+          <FIgrSplineSeries
+            name="series2"
+            valueMemberPath="China"
+            xAxisName="xAxis"
+            yAxisName="yAxis"
+          />
+          <FIgrStepLineSeries
+            name="series3"
+            valueMemberPath="Russia"
+            xAxisName="xAxis"
+            yAxisName="yAxis"
+          />
+        </FIgrDataChart>
 
-    return (
-      <div className="container sample">
-        <div className="container" style={{ height: "100%" }}>
-          <FIgrDataChart
-            ref={this.onChartRef}
-            width="800px"
-            height="500px"
-            dataSource={this.dummyData}
-            isHorizontalZoomEnabled={true}
-            isVerticalZoomEnabled={true}
-          >
-            <FIgrCategoryXAxis name="xAxis" label="X" />
-            <FIgrNumericYAxis name="yAxis" />
-            <FIgrStepLineSeries
-              name="series3"
-              valueMemberPath="Russia"
-              xAxisName="xAxis"
-              yAxisName="yAxis"
-            />
-          </FIgrDataChart>
+        <button onClick={handleHighlightButtonClick}>Highlight Series</button>
 
-          <FIgrDataChart
-            width="800px"
-            height="500px"
-            dataSource={this.dummyData}
-            isHorizontalZoomEnabled={true}
-            isVerticalZoomEnabled={true}
-            highlightingMode={this.highlightingMode}
-          >
-            <FIgrCategoryXAxis name="xAxis" label="X" />
-            <FIgrNumericYAxis name="yAxis" />
-            <FIgrLineSeries
-              name="series1"
-              title="USA"
-              valueMemberPath="USA"
-              xAxisName="xAxis"
-              yAxisName="yAxis"
-              thickness={series1Thickness}
-            />
-            <FIgrSplineSeries
-              name="series2"
-              valueMemberPath="China"
-              xAxisName="xAxis"
-              yAxisName="yAxis"
-            />
-            <FIgrStepLineSeries
-              name="series3"
-              valueMemberPath="Russia"
-              xAxisName="xAxis"
-              yAxisName="yAxis"
-            />
-          </FIgrDataChart>
-
-          <button onClick={this.handleHighlightButtonClick}>Highlight Series</button>
-
-          {/* 新しい state を元に表示する */}
-          <div>{renderingText}</div>
-        </div>
+        {/* 新しい state を元に表示する */}
+        <div>{renderingText}</div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default DataChartNext;
