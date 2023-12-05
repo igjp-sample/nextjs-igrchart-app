7491a21c5a9c6148fa6c5145dd84ef6e37f1e26e
MasakiTanoue <mtanoue@infragistics.com>
2023-12-05 14:08:48 +0900

    Add a comment

--- pages/CalendarNext.tsx
+++ pages/CalendarNext.tsx
@@ -10,10 +10,19 @@ const DataChartNext = () => {
   const [headerVisible, setHeaderVisible] = useState(true);
   const calendarRef = useRef(null);
 
+  // 【検証】ヘッダーをdisplay: none; として、左側に表示されるカレンダーの幅を広げる
+  //   <div part="header">　←　この要素に対して display: none; を反映することでヘッダーを非表示にしたい
+  //     <h5 part="header-title">
+  //     <slot name="title">Select range</slot>
+  //   </h5>
+  //   <h2 part="header-date"><span>12月12日</span>
+  //   <span> - </span>
+  //   <span>12月21日</span></h2>
+  //   </div>
   useEffect(() => {
-    // ここで条件に基づいてスタイルを変更
+    // width が600pxで反映されることは確認済み
     setCalendarStyle(prevStyle => ({
-      ...prevStyle,  // 既存のスタイルをコピー
+      ...prevStyle,
       width: '600px',
     }));
 
@@ -27,11 +36,13 @@ const DataChartNext = () => {
         headerElement.style.setProperty("display", "none", "important");
       }
     }
-
   }, []);
+  // 【検証結果1】with: 600px; は反映されるが、headerに対してdisplay: none; は反映されない
+  // 【検証結果2】important を付与しても反映されない（Reactに合わない）
 
   return (
     <div className="container sample" style={{ display: 'flex' }}>
+      {/* ここにカレンダーヘッダー部分のみを新たに表示したい */}
       <FIgrCalendar ref={calendarRef} style={calendarStyle} className={`header ${headerVisible ? '' : 'hide-element'}`}/>
       <p/>
     </div>
