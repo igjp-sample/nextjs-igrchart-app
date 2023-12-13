2bf12f34e56f7caa57e2a3297936c09c038267c4
MasakiTanoue <mtanoue@infragistics.com>
2023-12-13 15:08:37 +0900

    ViewDataButton add

--- pages/CalendarNext.tsx
+++ pages/CalendarNext.tsx
@@ -2,19 +2,62 @@ import React, { useState, useRef, useEffect } from "react";
 import { FIgrCalendar } from "../hooks/useCalendar";
 import "igniteui-webcomponents/themes/light/bootstrap.css";
 import "../styles/CalendarStyling.module.css";
+import { IgrCalendar } from "igniteui-react";
 
 const DataChartNext = () => {
   const [calendarStyle, setCalendarStyle] = useState({ width: '400px' });
-  const calendarRef = useRef(null);
+  const calendarRef = useRef<IgrCalendar>(null);
 
-  // Label名と一致するデータを設定
+  // refを設定するための関数
+  const onCalendarRef = (calendar: IgrCalendar) => {
+    if (!calendar) { return; }
+    calendarRef.current = calendar;
+  };
+
+  // aria-label の名称と一致するデータを作成
   const sampleData = [
+    '2023年12月5日火曜日',
     '2023年12月14日木曜日',
     '2023年12月20日水曜日',
+    '2023年12月25日月曜日',
   ];
 
+  // スタイルを適用する関数
+  const applyStylesAfterRendering = () => {
+    console.log("current",calendarRef.current);
+    if (calendarRef.current) {
+      // ShadowRoot DOMの階層を考慮して取得する
+      const igcCalendar = document.querySelector('igc-calendar');
+      const igcDaysView = document.querySelector('igc-calendar').shadowRoot.querySelector('igc-days-view');
+      const ariaLabel = document.querySelector('igc-calendar').shadowRoot.querySelector('igc-days-view').shadowRoot.querySelector(`[aria-label="2023年12月14日木曜日"]`);
+      console.log("igcCalendar",igcCalendar);
+      console.log("igcDaysView",igcDaysView);
+      console.log("ariaLabel",ariaLabel);
+
+      // スタイルを適用
+      sampleData.forEach(label => {
+        // 日付に対応するセルを取得
+        const target = document.querySelector('igc-calendar').shadowRoot.querySelector('igc-days-view').shadowRoot.querySelector(`[aria-label="${label}"]`);
+        if (target) {
+          console.log("HIT!",target);
+          // スタイルを適用
+          target.style.cssText = `
+            ${target.style.cssText};
+            background: yellow;
+          `;
+        }
+      });
+    }
+  };
+
+  // ViewDataボタンがクリックされたときの処理
+  const onViewDataButtonClick = () => {
+    console.log("ViewDataボタンがクリックされました。");
+    // スタイルを適用
+    applyStylesAfterRendering();
+  };
+
   useEffect(() => {
-    // FIgrCalendar コンポーネントがロードされるまで待機
     const fetchData = async () => {
       const { FIgrCalendar: DynamicFIgrCalendar } = await import("../hooks/useCalendar");
 
@@ -30,34 +73,27 @@ const DataChartNext = () => {
       }));
 
       // レンダリング後にサンプルデータに含まれる日付に対して強調表示のスタイルを適用
-      if (calendarRef.current) {
-        const calendarInstance = calendarRef.current.calendarRef.current;
-        if (calendarInstance) {
-          // ビューが変更されたらスタイルを適用
-          calendarInstance.onViewChange = () => {
-            sampleData.forEach(label => {
-              // 日付に対応するセルを取得
-              const cellElement = calendarInstance.calendarElement.querySelector(`[aria-label="${label}"]`);
-              if (cellElement) {
-                // スタイルを適用
-                cellElement.style.cssText = `
-                  ${cellElement.style.cssText};
-                  background: yellow;
-                `;
-              }
-            });
-          };
-        }
-      }
+      applyStylesAfterRendering();
     };
 
     fetchData();
-  }, [calendarRef]);
+  }, []);
 
   return (
-    <div className="container sample" style={{ display: 'flex' }}>
-      <FIgrCalendar ref={calendarRef} style={calendarStyle} />
+    <div className="container sample">
+      <FIgrCalendar ref={onCalendarRef} style={calendarStyle} />
       <p />
+
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", marginTop: "10px" }}>
+        {/* 日付データ表示ボタン */}
+        <div style={{ display: "flex", alignItems: "center", marginTop: "10px" }}>
+          <div style={{ width: "10em", fontWeight: "bold", marginRight: "1em" }}>別の日付データ表示</div>
+            <div style={{ display: "flex", alignItems: "center", marginTop: "10px" }}>
+              <button onClick={onViewDataButtonClick}>ViewData</button>
+            </div>
+        </div>
+      </div>
+
     </div>
   );
 };
